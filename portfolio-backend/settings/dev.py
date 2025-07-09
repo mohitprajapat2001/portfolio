c5889f8185cb1c01007fbe6161db1f4238d68f17
mohitprajapat2001 <108051766+mohitprajapat2001@users.noreply.github.com>
2025-07-09 17:47:48 +0530

    Settings Module Added

---
+++ portfolio-backend/settings/dev.py
@@ -0,0 +1,13 @@
+from settings.base import *  # noqa
+
+DEBUG = True
+ALLOWED_HOSTS = [
+    "*",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
